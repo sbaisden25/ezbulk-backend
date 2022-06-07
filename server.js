d57174fdb519ec19a2896a9fae84cd493ca1b842
sbaisden25 <spbaisden@gmail.com>
2022-06-06 23:18:47 -0400

    deploy

--- server.js
+++ server.js
@@ -10,6 +10,15 @@ const port = process.env.PORT || 5000;
 
 // CORS
 app.use(cors());
+const corsOptions ={
+  origin:'*', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+}
+app.options('*', cors(corsOptions)); // preflight OPTIONS; put before other routes
+app.listen(port, function(){
+  console.log('CORS-enabled web server listening on port 80');
+}); 
 
 require('dotenv').config();
 
@@ -34,7 +43,7 @@ const productsRouter = require('./routes/products');
 app.use('/products', productsRouter); 
 
 
-
+/* 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
-}); 
+});  */
